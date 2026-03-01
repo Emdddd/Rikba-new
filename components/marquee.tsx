19592262baef733a75e567d2bc7ed7b36aa15659
Emdddd <94723826+Emdddd@users.noreply.github.com>
2026-03-01 06:13:56 +0100

    Create marquee.tsx

---
+++ components/marquee.tsx
@@ -0,0 +1,14 @@
+export default function Marquee() {
+  return (
+    <section
+      style={{
+        padding: "40px",
+        textAlign: "center",
+        borderTop: "1px solid #eee",
+        borderBottom: "1px solid #eee",
+      }}
+    >
+      <p>Marquee section placeholder</p>
+    </section>
+  );
+}
