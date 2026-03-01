da9eef2ac717dc9160889ed9f14dff339a1461b1
Emdddd <94723826+Emdddd@users.noreply.github.com>
2026-03-01 06:15:27 +0100

    Create how-it-works.tsx

---
+++ components/how-it-works.tsx
@@ -0,0 +1,16 @@
+export default function HowItWorks() {
+  return (
+    <section
+      style={{
+        padding: "100px 20px",
+        textAlign: "center",
+      }}
+    >
+      <h2>How It Works</h2>
+
+      <p>
+        How it works placeholder content.
+      </p>
+    </section>
+  );
+}
