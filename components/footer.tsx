f65dc920fb5d88bc2bfe42aa3d189ec5f7d44736
Emdddd <94723826+Emdddd@users.noreply.github.com>
2026-03-01 06:18:42 +0100

    Create footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "60px 20px",
+        textAlign: "center",
+        borderTop: "1px solid #eee",
+      }}
+    >
+      <p>Footer placeholder</p>
+    </footer>
+  );
+}
